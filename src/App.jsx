678a50907282c1814a204be9ca7b9135312c377b
djoetker <jansiede@posteo.de>
2024-04-03 17:06:20 +0200

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     
-      <div>
-        <p>vite</p>
-      </div>
+     <Routes>
+      
+     </Routes>
      
   )
 }
