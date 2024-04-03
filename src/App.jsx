9d97aa45ddf55c18d96b8921c1a2d4fd870ee08f
djoetker <jansiede@posteo.de>
2024-04-03 17:29:52 +0200

    navbar layout start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout/Layout'
 
 function App() {
 
   return (
     
      <Routes>
-      
+        <Route path="/" element={<Layout/>}>
+          
+        </Route>
      </Routes>
      
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+function Navbar() {
+
+  return (
+    <div className="navbar_container">
+        <section className="brandname_container">
+          <p><span>un</span>social media</p>
+        </section>
+      
+    </div>
+  )
+};
+
+
+export default Navbar;
